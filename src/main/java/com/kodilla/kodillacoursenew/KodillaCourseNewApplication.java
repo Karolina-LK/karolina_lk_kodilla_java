61c2747259b67929bb36bb7fe6f905987ffb582c
Karolina Lubieniecka <lubieniecka@interia.pl>
2018-12-14 14:36:18 +0100

    First Commit

---
+++ src/main/java/com/kodilla/kodillacoursenew/KodillaCourseNewApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.kodillacoursenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseNewApplication.class, args);
+    }
+
+}
+
